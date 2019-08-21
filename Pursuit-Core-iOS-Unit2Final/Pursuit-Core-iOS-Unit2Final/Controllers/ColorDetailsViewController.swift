71e9cc250a8472abdbed9b0552d2fde0a08a150f
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-08-21 15:12:17 -0400

    add outlets and actions for sliders, labels, and steppers

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ColorDetailsViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ColorDetailsViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ColorDetailsViewController: UIViewController {
     
-      var colorSelectedDetail: Crayon!
+    var colorSelectedDetail: Crayon!
     
     @IBOutlet weak var colorSelected: UILabel!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    
+    @IBOutlet weak var greenValueLabel: UILabel!
+    
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var alphaStepperOutlet: UIStepper!
+    
+    @IBOutlet weak var currentAlpha: UILabel!
+    
+    @IBAction func colorRedSlider(_ sender: UISlider) {
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,3 +57,27 @@ class ColorDetailsViewController: UIViewController {
     */
 
 }
+//
+//@IBAction func sliderAction(_ sender: UISlider) {
+//    currentFontSize = CGFloat(sender.value)
+//    updateLabel()
+//    stepperOutlet.value = Double(sender.value)
+//
+//}
+//
+//@IBAction func stepperAction(_ sender: UIStepper) {
+//    currentFontSize = CGFloat(sender.value)
+//    updateLabel()
+//    sliderOutlet.value = Float(sender.value)
+//}
+//
+//
+//override func viewDidLoad() {
+//    super.viewDidLoad()
+//
+//    // Do any additional setup after loading the view.
+//}
+//
+//private func updateLabel() {
+//    changeFontSize.font = UIFont.systemFont(ofSize: currentFontSize)
+//}

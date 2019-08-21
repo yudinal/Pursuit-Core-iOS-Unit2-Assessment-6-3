f33728f3630f564ac0e92d12e5bf2c2bad31b96b
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-08-21 10:54:09 -0400

    Adds another view controller with 3 sliders and 3 labels and a view controller class

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/ColorDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ColorDetailViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Mr Wonderful on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailViewController: UIViewController {
+    
+   //let detailColor:Crayon!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

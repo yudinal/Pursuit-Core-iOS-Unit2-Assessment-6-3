5e4ad06274903e6e943ab9a34014f6a406248208
phoenixmcknight <phoenixmcknight@gmail.com>
2019-08-21 15:35:36 -0400

    adds functions to set the intitial values for the labels

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailColorViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailColorViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 
 class DetailColorViewController: UIViewController {
     @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var alphaStepper: UIStepper!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var colorName: UILabel!
     @IBOutlet weak var blueSliderLabel: UILabel!
     @IBOutlet weak var greenSliderLabel: UILabel!
@@ -18,17 +22,77 @@ class DetailColorViewController: UIViewController {
     
     @IBOutlet weak var redSliderLabel: UILabel!
     var passingInfo:Crayon!
+    var changingColors:UIColor!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpNamesOfLabels()
+       edgeCase()
+        setUpInitialValues()
+     
+       // changeView()
         // Do any additional setup after loading the view, typically from a nib.
     }
     func setUpNamesOfLabels() {
-      redSliderLabel.text = "Red"
-        blueSliderLabel.text = "Blue"
-        greenSliderLabel.text = "Green"
-        alphaSliderLabel.text = "Alpha"
-        resetButton.setTitle("Reset Button", for: .normal) 
+      redSliderLabel.text = arrayOfNamesForViewControllerLabels()[0]
+        blueSliderLabel.text = "\(arrayOfNamesForViewControllerLabels()[2]) : \(blueSlider.value)"
+        greenSliderLabel.text = "\(arrayOfNamesForViewControllerLabels()[1]) : \(greenSlider.value)"
+        alphaSliderLabel.text = "\(arrayOfNamesForViewControllerLabels()[3]) : \(alphaStepper.value)"
+        resetButton.setTitle(arrayOfNamesForViewControllerLabels()[4], for: .normal)
+        colorName.text = passingInfo.name
+    }
+    func setUpInitialValues() {
+        
+       
+        redSlider.value = Float(passingInfo.red)
+        greenSlider.value = Float(passingInfo.green)
+        blueSlider.value = Float(passingInfo.blue)
+        alphaStepper.value = Double(defaultAlpha())
+        view.backgroundColor = UIColor(red: CGFloat(redSlider!.value), green: CGFloat(greenSlider!.value), blue: CGFloat(blueSlider!.value), alpha: CGFloat(alphaStepper!.value))
+    }
+    
+    @IBAction func redSliderAction(_ sender: UISlider) {
+         redSlider.value = sender.value
+        changeUIColorWithSlider()
+        }
+    @IBAction func blueSliderAction(_ sender: UISlider) {
+        blueSlider.value = sender.value
+        changeUIColorWithSlider()
+    }
+    @IBAction func greenSliderAction(_ sender: UISlider) {
+        greenSlider.value = sender.value
+        changeUIColorWithSlider()
+    }
+    @IBAction func alphaStepperAction(_ sender: UIStepper) {
+        alphaStepper.value = sender.value
+        changeUIColorWithSlider()
+    }
+    @IBAction func resetButton(_ sender: UIButton) {
+        setUpInitialValues()
+    }
+    func changeUIColorWithSlider() {
+        view.backgroundColor = UIColor(red: CGFloat(redSlider!.value), green: CGFloat(greenSlider!.value), blue: CGFloat(blueSlider!.value), alpha: CGFloat(alphaStepper!.value))
+        let arrayOfLabels = [redSliderLabel,blueSliderLabel,greenSliderLabel,alphaSliderLabel,colorName]
+        if redSlider.value > 0.0 &&  greenSlider.value > 0.0 && blueSlider.value > 0.0  && alphaStepper.value > 0.5  {
+        
+            arrayOfLabels.forEach({$0?.textColor = .black})
+            
+        } else {
+            arrayOfLabels.forEach({$0?.textColor = .white})
+        }
+        
+    }
+    func edgeCase() {
+        if colorName.text == edgeCaseColors()[0] {
+            let arrayOfLabels = [redSliderLabel,blueSliderLabel,greenSliderLabel,alphaSliderLabel,colorName]
+            
+            arrayOfLabels.forEach({$0?.textColor = .white})
+            
+        }
+        
     }
     
 }
+
+

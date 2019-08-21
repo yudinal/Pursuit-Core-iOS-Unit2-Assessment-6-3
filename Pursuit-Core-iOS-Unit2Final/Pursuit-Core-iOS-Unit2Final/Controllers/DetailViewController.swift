173ce34d10772399472c8b9c7cacea667b1565dc
Ian <ian@Ians-MacBook-Pro.local>
2019-08-21 12:53:18 -0400

    added detailVC and linked storyboad elements to a new corresponding cocoa file

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Ian Cervone on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var colorName: UILabel!
+  
+  @IBAction func redSlider(_ sender: UISlider) {
+  }
+  @IBAction func greenSlider(_ sender: UISlider) {
+  }
+  @IBAction func blueSlider(_ sender: UISlider) {
+  }
+  
+  @IBAction func alphaStepper(_ sender: UIStepper) {
+  }
+  
+  @IBAction func resetButton(_ sender: UIButton) {
+  }
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

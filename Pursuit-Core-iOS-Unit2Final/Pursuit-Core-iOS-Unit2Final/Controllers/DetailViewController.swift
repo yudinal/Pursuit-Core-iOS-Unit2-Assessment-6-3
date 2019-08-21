cf0007e75a57ad60496d286643c0f58e0d67607f
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-08-21 12:09:55 -0400

    Set up working background color sliders.

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -20,31 +20,57 @@ class DetailViewController: UIViewController {
     
     var currentCrayon: Crayon!
     
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        redValueLabel.text = "Red: \(sender.value)"
+        view.backgroundColor = UIColor(displayP3Red: CGFloat(sender.value), green: CGFloat(greenSlider.value) , blue: CGFloat(blueSlider.value), alpha: 1)
+    }
     
-   
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenValueLabel.text = "Green: \(sender.value)"
+         view.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(sender.value) , blue: CGFloat(blueSlider.value), alpha: 1)
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func blueSliderChanged(_ sender: UISlider) {
+        blueValueLabel.text = "Blue: \(sender.value)"
+         view.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value) , blue: CGFloat(sender.value), alpha: 1)
+    }
+    
+    private func setMinMaxColorValues() {
         redSlider.minimumValue = 0.0
         redSlider.maximumValue = 1.0
         blueSlider.minimumValue = 0.0
         blueSlider.maximumValue = 1.0
         greenSlider.minimumValue = 0.0
         greenSlider.maximumValue = 1.0
+    }
+    
+    private func setUpCurrentColorStats(){
+        colorNameLabel.text = currentCrayon.name
         
         redSlider.value = Float(currentCrayon.red/255)
-        blueSlider.value = Float(currentCrayon.blue/255)
-        greenSlider.value = Float(currentCrayon.green/255)
-        
         redValueLabel.text = "Red: \(redSlider.value)"
+        
+        greenSlider.value = Float(currentCrayon.green/255)
         greenValueLabel.text = "Green: \(greenSlider.value)"
+        
+        blueSlider.value = Float(currentCrayon.blue/255)
         blueValueLabel.text = "Blue: \(blueSlider.value)"
-        colorNameLabel.text = currentCrayon.name
-        view.backgroundColor = UIColor(displayP3Red: CGFloat(currentCrayon.red/255), green: CGFloat(currentCrayon.green/255) , blue: CGFloat(currentCrayon.blue/255), alpha: 1)
-        
-        
-
+    }
     
+    private func setBackgroundColor() {
+        let currentCrayonBackgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value) , blue: CGFloat(blueSlider.value), alpha: 1)
+        view.backgroundColor = currentCrayonBackgroundColor
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        viewDidLoad()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setMinMaxColorValues()
+        setUpCurrentColorStats()
+        setBackgroundColor()
     }
     
 

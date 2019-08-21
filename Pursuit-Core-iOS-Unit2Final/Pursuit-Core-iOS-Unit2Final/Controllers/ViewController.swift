6e9c1182e293890c7240bd520a33a5d8638660f7
aglegaspi <lex83nyc@gmail.com>
2019-08-21 14:26:13 -0400

    refactored cellForRowAt in VC

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -34,16 +34,17 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let crayon = crayons[indexPath.row]
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "crayonColor") {
-            cell.textLabel?.text = crayon.name
-            cell.detailTextLabel?.text = "Hex Value: \(crayon.hex)"
             
             if crayon.hex == "#000000" {
                 cell.textLabel?.textColor = .white
                 cell.detailTextLabel?.textColor = .white
-                cell.backgroundColor = UIColor(red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1.0)
-            } else {
-                cell.backgroundColor = UIColor(red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1.0)
             }
+            
+            cell.textLabel?.text = crayon.name
+            cell.detailTextLabel?.text = "Hex Value: \(crayon.hex)"
+            cell.backgroundColor = UIColor(red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1.0)
+            
+            
             return cell
         }
         return UITableViewCell()

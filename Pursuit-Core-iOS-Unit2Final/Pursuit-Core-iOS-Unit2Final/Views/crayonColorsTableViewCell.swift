876f43393fb0bb6e76d4d86a8f597691da5dd59b
karyRmartinez <karymartinez@pursuit.org>
2019-08-21 10:40:03 -0400

    made a tableView in my storyBoard

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/crayonColorsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  crayonColorsTableViewCell.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Kary Martinez on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class crayonColorsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

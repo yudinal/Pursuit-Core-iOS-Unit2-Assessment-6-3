5646b91e0b9425d13dd02ef3e25dcba3cf902023
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-08-21 11:36:21 -0400

    adds navigation controller and detailed view controller. Adds segue identifier to segue.

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonsDetailedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CrayonsDetailedViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Mariel Hoepelman on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class CrayonsDetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

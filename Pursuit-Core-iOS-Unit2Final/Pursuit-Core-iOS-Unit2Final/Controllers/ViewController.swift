aa6800661e1805dfbf4cb9a341509b2708e2e3ce
Ian <ian@Ians-MacBook-Pro.local>
2019-08-21 12:11:00 -0400

    added func to set row color, also func to set row height

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -24,11 +24,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
       let boxColors = crayons[indexPath.row]
       cell.textLabel?.text = boxColors.name
       cell.detailTextLabel?.text = boxColors.hex
-      cell.backgroundColor = UIColor(
+      cell.backgroundColor = hexToRGB(hex: boxColors.hex)
     return cell
   }
 
-  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 65
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -39,3 +41,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
 }
 
+func hexToRGB (hex:String) -> UIColor {
+  var hexString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+  
+  if (hexString.hasPrefix("#")) {
+    hexString.remove(at: hexString.startIndex)
+  }
+  
+//  if ((hexString.count) != 6) {
+//    return UIColor.gray
+//  }
+  
+  var rgbValue:UInt64 = 0
+  Scanner(string: hexString).scanHexInt64(&rgbValue)
+  
+  return UIColor(
+    red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+    green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+    blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+    alpha: CGFloat(1.0)
+  )
+}

29a0885f2e22bb24584b294e2e7b709757d2c578
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-08-21 11:15:37 -0400

    configures labels texts

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonsViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonsViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 class CrayonsViewController: UIViewController {
     
     var crayons = Crayon.allTheCrayons
-    
+    @IBOutlet weak var crayonNameLabel: UILabel!
+    @IBOutlet weak var crayonHexValueLabel: UILabel!
+
     @IBOutlet weak var CrayonsTableVIew: UITableView!
+    @IBOutlet weak var crayonCell: UITableViewCell!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,10 +31,13 @@ extension CrayonsViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        let crayon = crayons[indexPath.row]
+        cell.textLabel?.text = crayon.name
+        cell.detailTextLabel?.text = crayon.hex
+//        cell.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red), green: CGFloat(crayon.green), blue: CGFloat(crayon.blue), alpha: 1.0)
+         return cell
     }
-    
-    
 }
 
 extension CrayonsViewController: UITableViewDelegate {

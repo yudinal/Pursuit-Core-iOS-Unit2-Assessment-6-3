38b2acc6aba04782bda9b552ef424e895be4c6c1
Michelle <Michelle.cueva@gmail.com>
2019-08-21 12:49:47 -0400

    Creates outlets and actions for detail view controller

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/DetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/DetailViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var colorNameLabel: UILabel!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    
+    @IBOutlet weak var greenValueLabel: UILabel!
+    
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var alphaValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var alphaStepper: UIStepper!
+    
+    @IBAction func redSliderTouched(_ sender: UISlider) {
+    }
+    
+    @IBAction func greenSliderTouched(_ sender: UISlider) {
+    }
+    
+    @IBAction func blueSliderTouched(_ sender: UISlider) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
     
 
     /*

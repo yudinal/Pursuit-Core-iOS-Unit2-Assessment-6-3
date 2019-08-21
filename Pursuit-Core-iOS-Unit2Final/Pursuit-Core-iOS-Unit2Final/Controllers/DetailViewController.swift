7dcb757af2ad9d66ea1e5d0822c5c7d800d3dc9f
aglegaspi <lex83nyc@gmail.com>
2019-08-21 11:47:11 -0400

    added outlets and actions to Detail View Controller

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -11,10 +11,27 @@ import UIKit
 class DetailViewController: UIViewController {
     var crayon: Crayon!
     
+    @IBOutlet weak var crayonName: UILabel!
+    
+    @IBAction func changeRedFloat(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeGreenFloat(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeBlueFloat(_ sender: UISlider) {
+    }
+    
+    @IBAction func changeAlpha(_ sender: UIStepper) {
+    }
+    
+    @IBAction func resetPressed(_ sender: UIButton) {
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         
     }

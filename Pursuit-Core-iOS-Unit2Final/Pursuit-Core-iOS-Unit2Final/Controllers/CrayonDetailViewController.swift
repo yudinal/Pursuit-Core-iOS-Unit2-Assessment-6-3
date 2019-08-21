ad76355d0bc2052fdbd648ff9ac90d6c3b3bc538
alysonabril <alysonabril@pursuit.org>
2019-08-21 13:35:21 -0400

    background changes colors

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonDetailViewController.swift
@@ -32,19 +32,25 @@ class CrayonDetailViewController: UIViewController {
         view.backgroundColor = UIColor(red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1)
     }
     
-    
+   
     
     @IBAction func redSlider(_ sender: UISlider) {
         
         redValueLabel.text = "Red:   \(String(format: "%.3f", sender.value))"
+        let redColor = UIColor(red: CGFloat(sender.value/255), green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: 1)
+        view.backgroundColor = redColor
     }
     
     @IBAction func greenSlider(_ sender: UISlider) {
         greenValueLabel.text = "Green:   \(String(format: "%.3f", sender.value))"
+        let greenColor = UIColor(red: CGFloat(crayon.red/255), green: CGFloat(sender.value/255), blue: CGFloat(crayon.blue/255), alpha: 1)
+        view.backgroundColor = greenColor
     }
     
     @IBAction func blueSlider(_ sender: UISlider) {
         blueValueLabel.text = "Blue:   \(String(format: "%.3f", sender.value))"
+        let blueColor = UIColor(red: CGFloat(crayon.red/255), green: CGFloat(crayon.green/255), blue: CGFloat(sender.value/255), alpha: 1)
+        view.backgroundColor = blueColor
     }
     
     

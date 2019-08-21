214f8f974f8674054b1d69fe87ddc19dd760dae3
Angela Garrovillas <angelagarrovillas@Angelas-MacBook-Pro.local>
2019-08-21 16:06:54 -0400

    put some func into separate file to make proper convenience init

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Models/hexCalculationFunc.swift
@@ -0,0 +1,9 @@
+//
+//  hexCalculationFunc.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Angela Garrovillas on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation

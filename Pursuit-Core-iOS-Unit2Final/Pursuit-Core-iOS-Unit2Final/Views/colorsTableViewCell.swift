227077ed9d095d6ee7b7b0b5f49afd5f807f3bde
karyRmartinez <karymartinez@pursuit.org>
2019-08-21 15:22:59 -0400

    not even sure im doing this right

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Views/colorsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  colorsTableViewCell.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Kary Martinez on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class colorsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

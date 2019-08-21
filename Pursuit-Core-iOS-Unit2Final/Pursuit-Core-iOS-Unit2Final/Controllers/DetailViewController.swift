920aac367f4a39b2a16ce36663a3753bb343f9ab
aglegaspi <lex83nyc@gmail.com>
2019-08-21 11:24:17 -0400

    created instance of Crayon in Detail View Controller

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    var crayon: Crayon!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
     }
 
 }

d5ce3f20410c8e8a1b01001810a626ec07e52e7c
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-08-21 10:40:00 -0400

    separates crayon data into new model

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Models/Crayon.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Models/Crayon.swift
@@ -21,22 +21,4 @@ class Crayon {
     self.blue = blue
     self.hex = hex
   }
-  static let allTheCrayons = [
-    Crayon(name: "Almond", red: 239, green: 222, blue: 205, hex: "#EFDECD"),
-    Crayon(name: "Antique Brass", red: 205, green: 149, blue: 117, hex: "#CD9575"),
-    Crayon(name: "Apricot", red: 253, green: 217, blue: 181, hex: "#FDD9B5"),
-    Crayon(name: "Aquamarine", red: 120, green: 219, blue: 226, hex: "#78DBE2"),
-    Crayon(name: "Asparagus", red: 135, green:  169, blue: 107, hex: "#87A96B"),
-    Crayon(name: "Atomic Tangerine", red: 255, green: 164, blue: 116, hex: "#FFA474"),
-    Crayon(name: "Banana Mania", red: 250, green: 231, blue: 181, hex: "#FAE7B5"),
-    Crayon(name: "Beaver", red: 159, green: 129, blue:  112, hex: "#9F8170"),
-    Crayon(name: "Bittersweet", red: 253, green: 124, blue: 110, hex: "#FD7C6E"),
-    Crayon(name: "Black", red: 0, green: 0, blue: 0, hex: "#000000"),
-    Crayon(name: "Blizzard Blue", red: 172, green: 229, blue: 238, hex: "#ACE5EE"),
-    Crayon(name: "Blue", red: 31, green: 117, blue: 254, hex: "#1F75FE"),
-    Crayon(name: "Blue Bell", red: 162, green: 162, blue: 208, hex: "#A2A2D0"),
-    Crayon(name: "Blue Gray", red: 102, green: 153, blue: 204, hex: "#6699CC"),
-    Crayon(name: "Blue Green", red: 13, green: 152, blue: 186, hex: "#0D98BA"),
-    Crayon(name: "Blue Violet", red: 115, green: 102, blue: 189, hex: "#7366BD")
-  ]
 }

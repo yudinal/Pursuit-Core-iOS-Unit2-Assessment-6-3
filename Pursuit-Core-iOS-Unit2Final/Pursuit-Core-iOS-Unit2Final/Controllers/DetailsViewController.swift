d2be8f0e45d9c3ecc71254f9b65fed1135f99832
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-21 10:44:33 -0400

    sets VC to navigation controller and creates new detailsVC in storyboard and detailsVC coco touch file

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Sunni Tang on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

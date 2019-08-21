adcbad772f8d486caf1c7019b7b77bd2f6aa19f2
Pursuit <pursuit@Pursuits-MacBook-Pro.local>
2019-08-21 11:33:26 -0400

    Set up Main Storyboard with TableView

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Pursuit on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    @IBOutlet weak var displayColorView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+}

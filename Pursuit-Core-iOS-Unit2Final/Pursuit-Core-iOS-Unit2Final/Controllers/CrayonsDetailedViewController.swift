bf801a774379f3e16583fb95dbc3b2055703ddfd
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-08-21 11:48:03 -0400

    adds labels, sliders, stepper and button to detailed  view controller.

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonsDetailedViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonsDetailedViewController.swift
@@ -6,14 +6,25 @@
 //  Copyright © 2019 Alex Paul. All rights reserved.
 //
 
+//When the user selects a row, you should segue to a Detail View. The Detail view should have:
+//A UILabel to show the name of the selected Crayon
+//A background color that starts as the same color as the selected Crayon.
+//A UISlider and UILabel that represents the current red value
+//A UISlider and UILabel that represents the current green value
+//A UISlider and UILabel that represents the current blue value
+//A UIStepper and UILabel that represents the current alpha
+//A reset UIButton that sets all the sliders and labels to be the corresponding colors of the Crayon the user selected and resets the alpha to 1.0.
+
 import UIKit
 
 class CrayonsDetailedViewController: UIViewController {
+    
+    //A UILabel to show the name of the selected Crayon
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //A background color that starts as the same color as the selected Crayon.
+        
     }
     
 

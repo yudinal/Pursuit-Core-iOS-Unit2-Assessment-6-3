6b790c436ca08951bfc9726b1213c6f4f11b4fbd
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-08-21 11:28:40 -0400

    table design and some controller functions

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ColorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ColorViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by albert coelho oliveira on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+    @IBOutlet weak var colorName: UILabel!
+    @IBOutlet weak var red: UILabel!
+    
+    @IBOutlet weak var redSlidderOut: UISlider!
+    @IBAction func redSlidderAct(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+
+}
--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    @IBOutlet weak var tableCellOut: UITableView!
+    var crayonTable = Crayon.allTheCrayons
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableCellOut.delegate = self
+        tableCellOut.dataSource = self
+    }
     
-  }
+    
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayonTable.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableCellOut.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
+        let selected = crayonTable[indexPath.row]
+        cell.textLabel?.text = selected.name
+        cell.detailTextLabel?.text =  selected.hex
+        cell.backgroundColor = UIColor.init(displayP3Red: CGFloat(selected.red/255), green: CGFloat(selected.green/255),
+            blue: CGFloat(selected.blue/255),
+            alpha: CGFloat(1.0))
+        return cell
+    }
 }
 

96db9fb436671bf0554587eda8f558ca07d24320
hildyabreu114 <hildyabreu@pursuit.org>
2019-08-21 11:33:25 -0400

    configured storyboard will tableView

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewControllerMain.swift
@@ -0,0 +1,28 @@
+//
+//  ViewControllerMain.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by hildy abreu on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerMain: UIViewController {
+
+    @IBOutlet weak var colorNameLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var hexValueLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}

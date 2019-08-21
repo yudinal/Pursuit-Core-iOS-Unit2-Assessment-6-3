8518ad0a95a53771ff54852df272640123e43798
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-08-21 10:45:29 -0400

    Adds a tableView  and populates all colors from the color to the cell

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    
 
+    @IBOutlet var crayonTableView: UITableView!
+    var crayonValue = Crayon.allTheCrayons
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    crayonTableView.delegate = self
+    crayonTableView.dataSource = self
   }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayonValue.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = crayonTableView.dequeueReusableCell(withIdentifier: "colorCell")
+        let info = crayonValue[indexPath.row]
+        cell?.textLabel?.text = info.name
+        cell?.detailTextLabel?.text = info.hex
+        cell?.backgroundColor = UIColor(red: CGFloat(info.red/255.0), green: CGFloat(info.green/255.0), blue: CGFloat(info.blue/255.0), alpha: 1)
+        return cell!
+    }
 
 }
 

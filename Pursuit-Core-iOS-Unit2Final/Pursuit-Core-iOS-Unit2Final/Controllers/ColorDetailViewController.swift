fbaa6b9dafc560b0521f236d5762a17c178256e4
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro.local>
2019-08-21 10:57:38 -0400

    Created second ViewController

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ColorDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ColorDetailViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Kevin Natera on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColorDetailViewController : UIViewController {
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}

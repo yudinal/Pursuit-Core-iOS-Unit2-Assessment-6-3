bfef83d2401fd90d5823c299689f110a202dd2df
aglegaspi <lex83nyc@gmail.com>
2019-08-21 10:39:35 -0400

    created an instance of Crayon model in View Controller

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -9,12 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let crayons = Crayon.allTheCrayons
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
 
-
 }
-

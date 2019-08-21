404d901292482c9ba4575e3293e7c334e4cdfc6e
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro.local>
2019-08-21 10:58:19 -0400

    Added TableView and populated it with Crayon data

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Crayon.allTheCrayons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableOutlet.dequeueReusableCell(withIdentifier: "colors", for: indexPath)
+        
+        cell.textLabel?.text = Crayon.allTheCrayons[indexPath.row].name
+        cell.detailTextLabel?.text = Crayon.allTheCrayons[indexPath.row].hex
+        cell.backgroundColor = UIColor(displayP3Red: CGFloat((Crayon.allTheCrayons[indexPath.row].red/255)), green: CGFloat((Crayon.allTheCrayons[indexPath.row].green/255)), blue: CGFloat((Crayon.allTheCrayons[indexPath.row].blue/255)), alpha: CGFloat(0.5))
+        
+        return cell
+    }
+    
+    
+    
+    
 
+    @IBOutlet weak var tableOutlet: UITableView!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    tableOutlet.dataSource = self
+    tableOutlet.delegate = self
   }
 
 

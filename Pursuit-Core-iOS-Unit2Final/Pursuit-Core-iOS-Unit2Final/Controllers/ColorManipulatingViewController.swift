00fa56c8cfd623283a53ab951dd6772c9f1d5fa7
NormanLiana <liananorman@pursuit.org>
2019-08-21 11:57:04 -0400

    adds color manipulating VC

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ColorManipulatingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorManipulatingViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Liana Norman on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ColorManipulatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

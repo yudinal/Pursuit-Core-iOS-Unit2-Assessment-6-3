35ab78348dede929c01f7f0a97d476b089402578
eeeric3995@yahoo.com <ihxx2s7tbu0>
2019-08-21 10:53:00 -0400

    Made my storyboard views

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -8,10 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    let color = Crayon.allTheCrayons
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return color.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "ourCell", for: indexPath)
+        
+        cell.textLabel?.text = color[indexPath.row].name
+        cell.detailTextLabel?.text = color[indexPath.row].hex
+        cell.contentView.backgroundColor = UIColor.init(red: CGFloat(color[indexPath.row].red), green: CGFloat(color[indexPath.row].green), blue: CGFloat(color[indexPath.row].blue), alpha: 10)
+        return cell
+    }
+    
+    
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    tableViewOutlet.delegate = self
+    tableViewOutlet.dataSource = self
     // Do any additional setup after loading the view, typically from a nib.
   }
 

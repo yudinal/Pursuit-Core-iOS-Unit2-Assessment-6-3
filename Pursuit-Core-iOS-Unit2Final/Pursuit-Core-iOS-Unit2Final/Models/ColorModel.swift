c7200ea2d3ec6d7253c608f81ec5a7ae7765613a
aglegaspi <lex83nyc@gmail.com>
2019-08-21 13:16:24 -0400

    create a model for Colors

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Models/ColorModel.swift
@@ -0,0 +1,37 @@
+//
+//  ColorModel.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Alexander George Legaspi on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorsModel {
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var alpha: CGFloat = 0.0
+    
+    func getColor() -> UIColor {
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    mutating func changeRed(red: CGFloat) {
+        self.red = red
+    }
+    
+    mutating func changeBlue(blue: CGFloat) {
+        self.blue = blue
+    }
+    
+    mutating func changeGreen(green: CGFloat) {
+        self.green = green
+    }
+    
+    mutating func changeAlpha(alpha: CGFloat) {
+        self.alpha = alpha
+    }
+}

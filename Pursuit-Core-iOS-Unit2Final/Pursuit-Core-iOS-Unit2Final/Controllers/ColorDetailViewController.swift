175ee2814d472de99a4ab2a2a5904b0d88d2670d
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-08-21 12:56:38 -0400

    allows sliders to change red, green and blue values of color selected from tableview (resets when you click back button)

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ColorDetailViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ColorDetailViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ColorDetailViewController: UIViewController {
     
     //MARK: - Properties
-      var color: Crayon!
+    var color: Crayon!
+    var colorSlider = ColorsForSlider()
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet var background: UIView!
@@ -25,12 +26,36 @@ class ColorDetailViewController: UIViewController {
     @IBOutlet weak var resetColorButton: UIButton!
     
     //MARK: - Functions
+    
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        switch sender.tag {
+        case 0:
+            colorSlider.changeRed(red: CGFloat(sender.value))
+            updateBackgroundColor()
+        case 1:
+            colorSlider.changeGreen(green: CGFloat(sender.value))
+            updateBackgroundColor()
+        case 2:
+            colorSlider.changeBlue(blue: CGFloat(sender.value))
+            updateBackgroundColor()
+        default:
+            print("How did this happen")
+        }
+    }
+    
+    func updateBackgroundColor() {
+        background.backgroundColor = colorSlider.getColor()
+    }
     func setUpView() {
         nameLabel.text = color.name
         redSlider.value = Float(color.red)/255
         greenSlider.value = Float(color.green)/255
         blueSlider.value = Float(color.blue)/255
         background.backgroundColor = UIColor(red: CGFloat(color.red)/255, green: CGFloat(color.green)/255, blue: CGFloat(color.blue)/255, alpha: 1.0)
+        colorSlider.changeRed(red: CGFloat(color.red)/255)
+        colorSlider.changeGreen(green: CGFloat(color.green)/255)
+        colorSlider.changeBlue(blue: CGFloat(color.blue)/255)
     }
     
     
---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Models/ColorsForSlider.swift
@@ -0,0 +1,40 @@
+//
+//  ColorsForSlider.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Radharani Ribas-Valongo on 8/21/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ColorsForSlider {
+    var red: CGFloat = 1
+    var green: CGFloat = 1
+    var blue: CGFloat = 1
+    var alpha: CGFloat = 1
+    
+    
+    func getColor() -> UIColor {
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    
+    mutating func changeRed(red: CGFloat) {
+        self.red = red
+    }
+    
+    mutating func changeBlue(blue: CGFloat) {
+        self.blue = blue
+    }
+    
+    
+    mutating func changeGreen(green: CGFloat) {
+        self.green = green
+    }
+    
+    mutating func changeAlpha(alpha: CGFloat) {
+        self.alpha = alpha
+    }
+}

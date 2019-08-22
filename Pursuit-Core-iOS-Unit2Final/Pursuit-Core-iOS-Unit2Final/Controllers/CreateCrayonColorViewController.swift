8a91623f8fb41d75a00e1f20298a66842735bed0
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-08-22 18:24:46 -0400

    Updated CreateColorVC with a name preview as well as limiting the inputs of the hexTextField to only receive hex characters up to a max of 6

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CreateCrayonColorViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CreateCrayonColorViewController.swift
@@ -12,6 +12,7 @@ class CreateCrayonColorViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var hexLabel: UILabel!
     
+    @IBOutlet weak var colorNamePreview: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var hexTextField: UITextField!
     @IBOutlet weak var colorPreviewView: UIView!
@@ -31,7 +32,32 @@ class CreateCrayonColorViewController: UIViewController {
 extension CreateCrayonColorViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        colorPreviewView.backgroundColor = UIColor(hex: hexTextField.text!)
+        switch textField.tag {
+        case 0: colorNamePreview?.text = textField.text
+        case 1:
+            colorPreviewView.backgroundColor = UIColor(hex: hexTextField.text!)
+        default:()
+        }
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        switch textField.tag {
+        case 0: ()
+        case 1: let aSet = NSCharacterSet(charactersIn:"abcdefABCDEF0123456789").inverted
+        let compSepByCharInSet = string.components(separatedBy: aSet)
+        let numberFiltered = compSepByCharInSet.joined(separator: "")
+        
+        if string == numberFiltered {
+            let currentText = textField.text ?? ""
+            guard let stringRange = Range(range, in: currentText) else { return false }
+            let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+            return updatedText.count <= 6
+        } else {
+            return false
+            }
+        default: ()
+        }
         return true
     }
 }

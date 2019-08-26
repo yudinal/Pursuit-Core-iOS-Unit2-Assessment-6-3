16fb2fa60cd6d811e5388a11c00f27a287d15694
Bee <biancabrown@pursuit.org>
2019-08-26 13:53:59 -0400

    Added Detailed View Controller to Storyboard and created a Cocoa Touch File for the View Controller.Set the subclass to the View Controller as well.

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailedColorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailedColorViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Bianca Brown on 8/26/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class DetailedColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

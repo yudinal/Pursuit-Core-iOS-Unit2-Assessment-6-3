3ecc10441dc16092dc5790c8c1d7eaa62a9cda81
Bee <biancabrown@pursuit.org>
2019-08-26 16:26:25 -0400

    Changed Background Color to reflect the color that is selected.

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailedColorViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/DetailedColorViewController.swift
@@ -13,6 +13,8 @@ class DetailedColorViewController: UIViewController {
     var color: Crayon!
     // this is the instance of the model that we are annotating
     
+    @IBOutlet weak var colorNameLabel: UILabel!
+    
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var redColorLabel: UILabel!
     
@@ -20,12 +22,9 @@ class DetailedColorViewController: UIViewController {
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var blueColorLabel: UILabel!
     
-    
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var greenColorLabel: UILabel!
-    
-    
-    
+
     @IBOutlet weak var alphaStepperChanger: UIStepper!
     @IBOutlet weak var alphaStepperLabel: UILabel!
     
@@ -37,6 +36,11 @@ class DetailedColorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorNameLabel.text = color.name
+        // this is to update the label that I have for the Color Name label to display when it is selected.
+        
+        view.backgroundColor = UIColor(displayP3Red: (CGFloat(color.red/255)), green: CGFloat(color.green/255), blue: CGFloat(color.blue/255), alpha: 1)
+        // this is to display the color as the background when it is selected from the view controller.
     }
 
 }

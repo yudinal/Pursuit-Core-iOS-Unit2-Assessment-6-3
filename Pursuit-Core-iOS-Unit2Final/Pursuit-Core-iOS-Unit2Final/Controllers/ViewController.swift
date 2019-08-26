e483043176462963dc839049fd63b655f96b0794
Bee <biancabrown@pursuit.org>
2019-08-26 09:47:27 -0400

    Added table view to view controller. Unsure on how to add the data to the table itself.

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var crayola = Crayon.allTheCrayons
+    
+    @IBOutlet weak var colorTableView: UITableView!
+   
+    override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        colorTableView.delegate = self
+        colorTableView.dataSource = self
+    
   }
 
 
 }
 
+extension ViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Crayon.allTheCrayons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let crayola = Crayon.allTheCrayons
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
